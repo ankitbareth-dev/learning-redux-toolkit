b8b8ef91e22d0fc9ec78d2955b2ee74db998ffe2
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-01-02 11:47:58 +0530

    provided the store to the entire app

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,25 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 
 import App from './App'
+import { store } from './app/store'
 
 import { worker } from './api/server'
 
 import './primitiveui.css'
 import './index.css'
 
-
 async function start() {
-  
   await worker.start({ onUnhandledRequest: 'bypass' })
 
   const root = createRoot(document.getElementById('root')!)
 
   root.render(
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>,
   )
 }
