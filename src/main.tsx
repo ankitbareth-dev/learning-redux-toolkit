5b72939ef6e23b8d4f199c0c040a4fd8125d267e
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-01-02 10:02:14 +0530

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -8,9 +8,9 @@ import { worker } from './api/server'
 import './primitiveui.css'
 import './index.css'
 
-// Wrap app rendering so we can wait for the mock API to initialize
+
 async function start() {
-  // Start our mock API server
+  
   await worker.start({ onUnhandledRequest: 'bypass' })
 
   const root = createRoot(document.getElementById('root')!)
