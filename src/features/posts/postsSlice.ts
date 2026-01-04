9012bc65d17927162a63f3a2076850d6004a1391
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-01-04 17:58:47 +0530

    extend user feature

--- src/features/posts/postsSlice.ts
+++ src/features/posts/postsSlice.ts
@@ -120,6 +120,11 @@ export default postsSlice.reducer
 
 export const selectAllPosts = (state: RootState) => state.posts.posts
 
+export const selectPostsByUser = (state: RootState, userId: string) => {
+  const allPosts = selectAllPosts(state)
+  return allPosts.filter((post) => post.user === userId)
+}
+
 export const selectPostById = (state: RootState, postId: string) => state.posts.posts.find((post) => post.id === postId)
 
 export const selectPostsStatus = (state: RootState) => state.posts.status
