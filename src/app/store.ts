8dfdaff54f1e3068d799eccb16207af839560e0d
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-01-04 18:44:06 +0530

    notification slice added

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import postsReducer from '@/features/posts/postsSlice'
 import usersReducer from '@/features/users/usersSlice'
 import authReducer from '@/features/auth/authSlice'
+import notificationReducer from '@/features//notifications/notificationsSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     posts: postsReducer,
     users: usersReducer,
+    notifications: notificationReducer,
   },
 })
 
