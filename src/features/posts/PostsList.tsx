6bb7b028adf67dad3dba731bcbd58e5407683cd9
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-01-04 11:26:27 +0530

    added a loading spinner

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -1,15 +1,40 @@
-import { useAppDispatch, useAppSelector } from '@/app/hooks'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { fetchPosts, selectAllPosts, selectPostsStatus } from './postsSlice'
-import { PostAuthor } from './PostAuthor'
+
+import { useAppSelector, useAppDispatch } from '@/app/hooks'
+
+import { Spinner } from '@/components/Spinner'
 import { TimeAgo } from '@/components/TimeAgo'
+
+import { PostAuthor } from './PostAuthor'
 import { ReactionButtons } from './ReactionButtons'
-import { useEffect } from 'react'
+import { Post, selectAllPosts, selectPostsError, fetchPosts, selectPostsStatus } from './postsSlice'
+
+interface PostExcerptProps {
+  post: Post
+}
+
+function PostExcerpt({ post }: PostExcerptProps) {
+  return (
+    <article className="post-excerpt" key={post.id}>
+      <h3>
+        <Link to={`/posts/${post.id}`}>{post.title}</Link>
+      </h3>
+      <div>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+      </div>
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+      <ReactionButtons post={post} />
+    </article>
+  )
+}
 
 export const PostsList = () => {
   const dispatch = useAppDispatch()
   const posts = useAppSelector(selectAllPosts)
   const postStatus = useAppSelector(selectPostsStatus)
+  const postsError = useAppSelector(selectPostsError)
 
   useEffect(() => {
     if (postStatus === 'idle') {
@@ -17,24 +42,22 @@ export const PostsList = () => {
     }
   }, [postStatus, dispatch])
 
-  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+  let content: React.ReactNode
 
-  const renderedPosts = orderedPosts.map((post) => (
-    <article className="post-excerpt" key={post.id}>
-      <h3>
-        <Link to={`/posts/${post.id}`}>{post.title}</Link>
-      </h3>
-      <p className="post-content">{post.content.substring(0, 100)}</p>
-      <PostAuthor userId={post.user} />
-      <TimeAgo timestamp={post.date} />
-      <ReactionButtons post={post} />
-    </article>
-  ))
+  if (postStatus === 'pending') {
+    content = <Spinner text="Loading..." />
+  } else if (postStatus === 'succeeded') {
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+
+    content = orderedPosts.map((post) => <PostExcerpt key={post.id} post={post} />)
+  } else if (postStatus === 'failed') {
+    content = <div>{postsError}</div>
+  }
 
   return (
     <section className="posts-list">
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   )
 }
